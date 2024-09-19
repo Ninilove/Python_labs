79e7a6ca4cb525071c2e824a189a3791a68e8c97
Nina <ngouabou46@gmail.com>
2024-09-19 15:24:53 -0400

    add lab 1 and lab 2 to the repository

---
+++ Lab1_format_method.py
@@ -0,0 +1,14 @@
+# Define a variable called first_name using a string
+first_name = "Nina"
+
+# Define a variable representing your age using an integer
+age = "37"
+
+# Represent your favorite color using a string
+
+favorite_color = "Turquoise"
+
+# Using the format() method, display a sentence in the following format
+sentence = "Hello, my name is {}. I am {} years old, and my favorite color is {}".format(first_name, age, favorite_color)
+
+print (sentence)
---
+++ Lab2_f-string_method.py
@@ -0,0 +1,10 @@
+# define a variable containing a string as the name of a city
+city = "Silver Spring"
+
+# define a variable with a floating-point representing the current temperature in Celcius
+temperature = "26.11 °C"
+
+# use the f-string method to display the sentence
+sentence = f"The current temperature in {city} is {temperature}"
+
+print(sentence)
