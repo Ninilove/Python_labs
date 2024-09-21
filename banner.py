ab3cc510c1e5b3f23f69671ea4f678d53534f0cf
Nina <ngouabou46@gmail.com>
2024-09-20 23:16:48 -0400

    updated banner.py file using the f-string at the print statement

--- banner.py
+++ banner.py
@@ -1,13 +1,9 @@
 
-name = 'Nina  #'
+name = 'Nina   #'
 top_banner_var = "#" * 38
 side_banner_var = "# \t\t\t\t     #"
 side_banner_var1 = "# WELCOME TO STREET FIGHTER:  "
 side_banner_var2 = "# \t\t\t\t     #"
 bottom_banner_var = "#" * 38
 
-print (top_banner_var)
-print (side_banner_var)
-print (side_banner_var1 , name)
-print (side_banner_var2)
-print (bottom_banner_var)
+print (f"{top_banner_var}\n{side_banner_var}\n{side_banner_var1}{name}\n{side_banner_var2}\n{bottom_banner_var}")
