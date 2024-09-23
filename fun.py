6ce4aaee81318c5d81fa302615a9719b086dabaa
Nina <ngouabou46@gmail.com>
2024-09-23 15:15:23 -0400

    Add new code for CakeLabs project

---
+++ fun.py
@@ -0,0 +1,17 @@
+# My next vacation destination is
+city = "Ubud"
+
+
+# The gaz price in my neighborhood is
+price = 3.39
+
+# The highest winning score for the Lakers in their history (1971-1972) [ps: this is for you Claudio] was
+high_score = 162
+
+# Is is true or false that Nina is having fun doing the Python boot camp at Cloud Space Academy?
+is_having_fun = True
+
+print ('My next vacation destination is', city)
+print ('The gaz price in my neighborhood is', price)
+print ('The highest winning score for the Lakers in their history (1971-1972) [ps: this is for you Claudio] was', high_score)
+print ('Is is true or false that Nina is having fun doing the Python boot camp at Cloud Space Academy?', is_having_fun)
