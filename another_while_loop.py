1ce93db48fde8bda18a18d2d6eb51ef6451993d1
Nina <ngouabou46@gmail.com>
2024-10-15 23:06:51 -0400

    adding another while loop example starting with count 0

---
+++ another_while_loop.py
@@ -0,0 +1,18 @@
+# Write a while-loop and inside of your while-loop count down backward from 10 to 0
+# Print starting to countdown backward from 10-0
+# Print countdown is over! At the end 
+
+
+# starting with 0
+n = 0
+print ("Starting countdown backward from 10-0")
+
+# the loop will run until it n reaches 10
+while n <= 10:
+    n = n + 1 
+    
+    print(11 - n) # since n is counting up from 0, subtracting n from 11 prints the countdown
+    
+
+    
+print ("Countdown is over!") 
