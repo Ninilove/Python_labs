840574b0d038b7f5741a6f651196b41253dcb23d
Nina <ngouabou46@gmail.com>
2024-10-15 22:20:22 -0400

    add while loop to repo

---
+++ while_loop.py
@@ -0,0 +1,11 @@
+# Write a while-loop and inside of your while-loop count down backward from 10 to 0
+# Print starting to countdown backward from 10-0
+# Print countdown is over! At the end 
+
+n = 11
+print ("Starting countdown backward from 10-0")
+while n >= 1:
+    n = n - 1
+    print(n)
+    
+print ("Countdown is over!")   
